a2e03105b623db117c8f522d88d1badacd93548a
Andrew Wesie <awesie@gmail.com>
2016-08-23 23:05:39 -0400

    Update docs to match header

--- docs/html/search/all_8.js
+++ docs/html/search/all_8.js
@@ -3,7 +3,7 @@ var searchData=
   ['immediateblind',['ImmediateBlind',['../builtins_8h.html#ac08a1bd4356cff850e6fdd4c0ebd50f1',1,'builtins.h']]],
   ['inttostring',['IntToString',['../builtins_8h.html#ae5db86b1b573a674218b7d81248485d2',1,'builtins.h']]],
   ['isattackedby',['IsAttackedBy',['../builtins_8h.html#ad545b24b85a91b5db0b8806ba9aa4fdb',1,'builtins.h']]],
-  ['iscaller',['IsCaller',['../builtins_8h.html#a0803e508c56435f4928b1d78c49ea1e6',1,'builtins.h']]],
+  ['iscaller',['IsCaller',['../builtins_8h.html#a076a47a08e04b2ec41d43bee8b9299c1',1,'builtins.h']]],
   ['iscrown',['IsCrown',['../builtins_8h.html#a90534276970eeb8d8fa7cf28285b2adf',1,'builtins.h']]],
   ['isgameball',['IsGameBall',['../builtins_8h.html#a01c83acfc0b41ec8921e95f9349f7907',1,'builtins.h']]],
   ['islocked',['IsLocked',['../builtins_8h.html#ad1c783fe54ef73735c95e5eb15684e39',1,'builtins.h']]],
@@ -13,7 +13,7 @@ var searchData=
   ['issummoned',['IsSummoned',['../builtins_8h.html#ab91ebd47483cafded2b2c96fab912645',1,'builtins.h']]],
   ['istalking',['IsTalking',['../builtins_8h.html#a37e35c7dfaa1ebb9927f5826c7b1e719',1,'builtins.h']]],
   ['istrading',['IsTrading',['../builtins_8h.html#a2c42ae661665ca59d226ce0e1c75b6d0',1,'builtins.h']]],
-  ['istrigger',['IsTrigger',['../builtins_8h.html#a8d66743f42869fc0a99a83e921c94fb0',1,'builtins.h']]],
+  ['istrigger',['IsTrigger',['../builtins_8h.html#aa2167dbf9fc08a50260fca98c1d1388f',1,'builtins.h']]],
   ['isvisibleto',['IsVisibleTo',['../builtins_8h.html#a83a98f79211a5b4f9a35479f68bb34a7',1,'builtins.h']]],
   ['iswaypointon',['IsWaypointOn',['../builtins_8h.html#a00a1d901a15f7d2146aefd68bdca362f',1,'builtins.h']]]
 ];
--- docs/html/search/all_f.js
+++ docs/html/search/all_f.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['raise',['Raise',['../builtins_8h.html#ae6707a531d5b6df49ed94d96763e1ae0',1,'builtins.h']]],
-  ['raisezombie',['RaiseZombie',['../builtins_8h.html#a00d5b65e379968590ad6b59cc013f28c',1,'builtins.h']]],
+  ['raisezombie',['RaiseZombie',['../builtins_8h.html#a3a8da1d27f1ed1012e88830982dd86ba',1,'builtins.h']]],
   ['raisezombiegroup',['RaiseZombieGroup',['../builtins_8h.html#afb9319d448c50aa90fbc01ce6cc44058',1,'builtins.h']]],
   ['random',['Random',['../builtins_8h.html#a528079c9c91fd020b6d3ed1eac8bf530',1,'builtins.h']]],
   ['randomfloat',['RandomFloat',['../builtins_8h.html#a9f58d4bd6816fa021e202738ae24020f',1,'builtins.h']]],
